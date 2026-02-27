b070c661dfbae2d6337dbbd0a1e866d5f10438f9
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-02-27 09:28:26 -0300

    feat: created App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import BackgroundEffects from "./components/BackgroundEffect"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Expertise from "./components/Expertise"
+import TechStack from "./components/TechStack"
+import SelectedWorks from "./components/SelectedWorks"
+import CallToAction from "./components/CallToAction"
+import Footer from "./components/Footer"
+import Publications from "./components/Publications"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BackgroundEffects />
+      <Navbar />
+      <main className="max-w-7xl mx-auto px-4 md:px-8 py-6 md:py-10">
+        <Hero />
+        <Expertise />
+        <TechStack />
+        <SelectedWorks />
+        <Publications />
+        <CallToAction />
+        <Footer />
+      </main>
     </>
   )
 }
